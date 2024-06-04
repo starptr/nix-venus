b72a2e38d315f4753f09abd947109b34749b8280
Yuto Nishida <yuto@berkeley.edu>
2024-06-03 20:30:06 -0700

    Add bat

--- home-yuto.nix
+++ home-yuto.nix
@@ -154,6 +154,10 @@ in
     userName = "Yuto Nishida";
   };
 
+  programs.bat = {
+    enable = true;
+  };
+
   # Let Home Manager install and manage itself.
   # programs.home-manager.enable = true;
 
